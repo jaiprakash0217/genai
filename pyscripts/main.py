8d7d0e4bf0e18ffa6be75ef15c96ea89bbec0437
Jai Prakash <73898052+jaiprakash0217@users.noreply.github.com>
2024-10-23 17:38:40 +0530

    Update main.py

--- pyscripts/main.py
+++ pyscripts/main.py
@@ -1,6 +1,4 @@
 from pyscript import Element
-from pyscript import config
-print(config.get("files"))
 def print_hello(event):
         Element("output").write("Hello, World!")
         Element("helloBtn").element.onclick = print_hello
