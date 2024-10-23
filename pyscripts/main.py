8dd8da01a662ff9313bf82690e0dbaea56e3feed
Jai Prakash <73898052+jaiprakash0217@users.noreply.github.com>
2024-10-23 14:16:30 +0530

    Update main.py

--- pyscripts/main.py
+++ pyscripts/main.py
@@ -1,8 +1,6 @@
-        from pyscript import Element
-
-        # Function to print "Hello, World!" (it must accept the event argument)
-        def print_hello(event):
-            Element("output").write("Hello, World!")
-
-        # Bind the print_hello function to the button's click event
+from pyscript import Element
+from pyscript import config
+print(config.get("files"))
+def print_hello(event):
+        Element("output").write("Hello, World!")
         Element("helloBtn").element.onclick = print_hello
