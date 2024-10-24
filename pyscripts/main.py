b4b5f10ac617ed52e3a4159a0ab3c34c2bdb8dce
Jai Prakash <73898052+jaiprakash0217@users.noreply.github.com>
2024-10-24 20:26:03 +0530

    Update main.py

--- pyscripts/main.py
+++ pyscripts/main.py
@@ -2,4 +2,3 @@ print("hello world!")
 from pyscript import Element
 def print_hello(event):
         Element("output").write("Hello, World!")
-        Element("helloBtn").element.onclick = print_hello
